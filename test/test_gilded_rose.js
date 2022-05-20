20dbae0976f1634e3f969b957aa5e1ca3fd059b8
Giacomo Grandi <jak.grandi@gmail.com>
2022-05-20 12:17:34 +0300

    tests for aged brie

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -41,3 +41,23 @@ describe("Tests for normal items", function () {
     expect(items[1].quality).to.equal(0);
   });
 });
+
+describe("Tests for Aged Brie", function () {
+  it("should increase quality by 1 when sellIn is >= 0", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(11);
+  });
+
+  it("should increase quality by 2 when sellIn is < 0", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 0, 10)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(12);
+  });
+
+  it("quality should never be greater than 50", function () {
+    const gildedRose = new Shop([new Item("Aged Brie", 1, 50)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(50);
+  });
+});
